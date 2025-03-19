138e3bd5aedb80b8b91030c946ed3881122efee0
Ali Minagar <aminagar@gmail.com>
2025-03-19 05:35:24 -0500

    Initial commit - Astro website

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/astro-website/",  // 👈 Add your repository name here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
